219832487556bc054e3990bbae8e7ff57dd76f0f
Chirag Chakraborty <chiragchakraborty48@gmail.com>
2023-11-17 15:12:18 +0530

    Blogs Page

--- blogs.php
+++ blogs.php
@@ -21,8 +21,36 @@ include "partials/head.php";
       <div class="container">
         <h1 tabindex="0">Latest Blogs:</h1>
         <div class="blogs">
-          <h3 tabindex="0">Blog 1</h3>
-          <h3 tabindex="0">Blog 2</h3>
+          <div class="blog" tabindex="0">
+            <h1 tabindex="0">Blog Title</h1>
+            <p tabindex="0">By Author</p>
+            <p tabindex="0">Blog Summary - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, fugit voluptatem accusamus deserunt soluta hic. Soluta vero nisi aperiam praesentium!</p>
+            <a href="#">Read More</a>
+          </div>
+          <div class="blog" tabindex="0">
+            <h1 tabindex="0">Blog Title</h1>
+            <p tabindex="0">By Author</p>
+            <p tabindex="0">Blog Summary - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, fugit voluptatem accusamus deserunt soluta hic. Soluta vero nisi aperiam praesentium!</p>
+            <a href="#">Read More</a>
+          </div>
+          <div class="blog" tabindex="0">
+            <h1 tabindex="0">Blog Title</h1>
+            <p tabindex="0">By Author</p>
+            <p tabindex="0">Blog Summary - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, fugit voluptatem accusamus deserunt soluta hic. Soluta vero nisi aperiam praesentium!</p>
+            <a href="#">Read More</a>
+          </div>
+          <div class="blog" tabindex="0">
+            <h1 tabindex="0">Blog Title</h1>
+            <p tabindex="0">By Author</p>
+            <p tabindex="0">Blog Summary - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, fugit voluptatem accusamus deserunt soluta hic. Soluta vero nisi aperiam praesentium!</p>
+            <a href="#">Read More</a>
+          </div>
+          <div class="blog" tabindex="0">
+            <h1 tabindex="0">Blog Title</h1>
+            <p tabindex="0">By Author</p>
+            <p tabindex="0">Blog Summary - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, fugit voluptatem accusamus deserunt soluta hic. Soluta vero nisi aperiam praesentium!</p>
+            <a href="#">Read More</a>
+          </div>
         </div>
       </div>
     </div>
--- partials/sidebar.php
+++ partials/sidebar.php
@@ -5,4 +5,8 @@
         <a href="gallery.php">Photo Gallery</a>
         <a href="#">Admin Login</a>
     </div>
+    <div class="site-info">
+        <p tabindex="0">Blog Count: 5</p>
+        <p tabindex="0">Daily Visitors: 150</p>
+    </div>
 </div>
